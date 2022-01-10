e071286688f28d2c8afa1da35431ce2e02f2e17e
jarbasai <jarbasai@mailfence.com>
2022-01-10 20:14:05 +0000

    :tada:

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+# skill_id=package_name:SkillClass
+PLUGIN_ENTRY_POINT = 'skill-movie-central.jarbasai=skill_movie_central:MovieCentralSkill'
+
+setup(
+    # this is the package name that goes on pip
+    name='ovos-skill-movie-central',
+    version='0.0.1',
+    description='ovos movie central skill plugin',
+    url='https://github.com/JarbasSkills/skill-movie-central',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    package_dir={"skill_movie_central": ""},
+    package_data={'skill_movie_central': ['locale/*', 'ui/*']},
+    packages=['skill_movie_central'],
+    include_package_data=True,
+    install_requires=["ovos_workshop~=0.0.5a1"],
+    keywords='ovos skill plugin',
+    entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
+)
